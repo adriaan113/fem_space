d6e36966261a934384203d45c03d4dafe2af169b
Adriaan van der Ploeg <adriaan@air-van-adriaan.home>
2021-11-16 09:10:32 +0000

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,32 @@
 <template>
   <div id="app">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <!-- <img :src="bg" alt=""> -->
-
-    <!-- <h1>{{test}}</h1> -->
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    // HelloWorld
-  },
-  data(){
-    return {
-      test: "nenenenene",
-    }
-    
-  },
-  computed:{
-  },
-}
-</script>
-
-<style lang="scss">
-
-$medium:768px;
-$large: 1440px;
-
-*{
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  width: 100%;
-  height: 100vh;
-
-  background-image: url("./assets/home/background-home-mobile.jpg");
-  background-size: cover;
-  background-repeat: no-repeat;
-
-  @media (min-width: $medium) {
-     background-image: url("./assets/home/background-home-tablet.jpg");
-  }
-
-  @media (min-width: $large) {
-     background-image: url("./assets/home/background-home-desktop.jpg");
-  }
 }
 
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
