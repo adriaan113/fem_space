6b9177ac1776e41051517a5677e4419a339d8dea
Adriaan van der Ploeg <adriaan@air-van-adriaan.home>
2021-11-18 12:53:29 +0000

    added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,85 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+
+     <v-app-bar dark>
+        <v-img height="100%" contain position="left center" :src="logo"></v-img>
+        <v-spacer></v-spacer>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        
+        <!-- <v-toolbar-title>My files</v-toolbar-title> -->
+  
+       
+  
+      </v-app-bar>
+
+      <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        right
+        temporary
+      >
+        <v-list
+          nav
+        >
+        <v-btn
+            icon
+            @click.stop="drawer = !drawer"
+            right
+            absolute
+        >
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item>
+              <v-list-item-title>Foo</v-list-item-title>
+            </v-list-item>
+  
+            <v-list-item>
+              <v-list-item-title>Bar</v-list-item-title>
+            </v-list-item>
+  
+            <v-list-item>
+              <v-list-item-title>Fizz</v-list-item-title>
+            </v-list-item>
+  
+            <v-list-item>
+              <v-list-item-title>Buzz</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+
+
+    <v-main>
+      <router-view/>
+    </v-main>
+
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  data: () => ({
+    drawer: false,
+    group: null,
+    logo: require("./assets/space-logo.svg"),
+  }),
+  watch:{
+    group () {
+      this.drawer = false
+    },
+  }
+};
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="scss">
+
+
 </style>
